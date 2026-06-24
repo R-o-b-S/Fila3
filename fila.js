d715e581f15ceae9699735d9e23abafc021089f8
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-06-24 18:52:08 +0200

    players constructor, scoring system

--- fila.js
+++ fila.js
@@ -14,19 +14,25 @@ const gameBoard = [
 // 3 4 5
 // 6 7 8
 
-const players = ["player1", "player2"];
+function createPlayers (i) {
+    const name = "player "+i;
+    let score = 0;
+    return {name, score};
+}
+
+const players = [createPlayers(1),createPlayers(2),];
 
 let turn = Math.floor(Math.random() * 2);
 function activePlayer () { //cycle turns between players 
     if (turn === 0) {
         turn = 1;
         updateActivePlr(turn);
-        return players[0];
+        return players[0].name;
     }
     else if (turn === 1) {
         turn = 0;
         updateActivePlr(turn);
-        return players[1];
+        return players[1].name;
     }
 }
 
@@ -35,17 +41,17 @@ function updateActivePlr(t) { //update the displayed active player
     element.remove();
     const p = document.createElement("p");
     p.id = "acti";
-    const txt = "It's " + players[t] + " turn!";
+    const txt = "It's " + players[t].name + " turn!";
     p.textContent = txt;
     document.getElementById("activePlr").appendChild(p);
 }
 
 
 function playRound (player, box) {  //play a round for the active player
-    if (player === players[0]) {
+    if (player === players[0].name) {
         gameBoard[box] = "X";
         }
-    else if (player === players[1]) {
+    else if (player === players[1].name) {
         gameBoard[box] = "O";
         }
 }
@@ -59,7 +65,7 @@ function chkWin () { //check if the game is won by one of the players
         gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard [6] === "X" ||
         gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard [7] === "X" ||
         gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard [8] === "X") {
-            window.alert(players[0] + " wins!");
+            window.alert(players[0].name + " wins!");
             //the following prevents player to keep the game after someone won
             for (i=0; i<9; i++) {  
                 if (gameBoard[i] === "empty") {
@@ -73,9 +79,11 @@ function chkWin () { //check if the game is won by one of the players
             element.remove();
             const p = document.createElement("p");
             p.id = "acti";
-            const txt = players[0] + " won!";
+            const txt = players[0].name + " won!";
             p.textContent = txt;
             document.getElementById("activePlr").appendChild(p);
+            //the following increases the winner score
+            scoreRefresh (0);
         }
     else if (gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard [2] === "O" ||
         gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard [5] === "O" ||
@@ -85,7 +93,7 @@ function chkWin () { //check if the game is won by one of the players
         gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard [6] === "O" ||
         gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard [7] === "O" ||
         gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard [8] === "O") {
-            window.alert(players[1] + " wins!");
+            window.alert(players[1].name + " wins!");
             // the following prevents player to keep the game after someone won
             for (i=0; i<9; i++) { 
                 if (gameBoard[i] === "empty") {
@@ -99,9 +107,11 @@ function chkWin () { //check if the game is won by one of the players
             element.remove();
             const p = document.createElement("p");
             p.id = "acti";
-            const txt = players[0] + " won!";
+            const txt = players[0].name + " won!";
             p.textContent = txt;
             document.getElementById("activePlr").appendChild(p);
+            //the following increases the winner score
+            scoreRefresh (1);
         }
 }
 
@@ -145,6 +155,29 @@ function newGame () { //starts a new game
     displayBoard();
     turn = Math.floor(Math.random() * 2);
     updateActivePlr(turn);
+}
+
+function scoreRefresh (w) { //add score points and refresh value on screen
+    players[w].score= players[w].score+1;
+    const txt = "Score: " + players[w].score;
+    if (w === 0){
+        const element = document.getElementById("oneS");
+        element.remove();
+        const div = document.createElement("div");
+        div.classList = "score";
+        div.id = "oneS";
+        div.textContent = txt;
+        document.getElementById("plyr1").appendChild(div);
+        }
+    else if (w === 1){
+        const element = document.getElementById("twoS");
+        element.remove();
+        const div = document.createElement("div");
+        div.classList = "score";
+        div.id = "twoS";
+        div.textContent = txt;
+        document.getElementById("plyr2").appendChild(div);
+        }
 }
 
 displayBoard(); //needed to display the board on page loading
